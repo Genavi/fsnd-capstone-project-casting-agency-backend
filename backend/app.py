dda19c40fcd06e8d3bad04fd38f8141b4bd859f6
David Ringgenberg <david.ringgenberg@ringgi.net>
2020-06-11 23:06:46 +0200

    refactor: Remove MovieToArtist
    In models.py; removed the MovieToArtist class and replaced it with a new table called movieArtists.
    No more import of the MovieToArtist needed.

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from models import setup_db, Movie, Artist, MovieToArtist
+from models import setup_db, Movie, Actor, db
 
 def create_app(test_config=None):
   # create and configure the app
